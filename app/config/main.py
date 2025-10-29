7582a49d1111b91f23117184b71d5517ba394473
Phat Ha <pattrickha@gmail.com>
2025-10-29 18:21:19 +0700

    Fix config file.

--- app/config/main.py
+++ app/config/main.py
@@ -1,9 +1,30 @@
+from functools import lru_cache
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+DOTENV = os.path.join("app/", ".env")
 
 
 class Settings(BaseSettings):
     app_name: str = "Exam API"
-    postgres_url: str
+    postgres_url: str = ""
     log_level: str = "INFO"
 
-    model_config = SettingsConfigDict(env_file=(".env", ".env.prod"))
+    model_config = SettingsConfigDict(
+        env_file=DOTENV
+    )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+if __name__ == "__main__":
+    config = get_settings()
+    print(config)
+    print("Configuration Loaded:")
+    print(f"{config.app_name}")
+    print(f"{config.postgres_url}")
+    print(f"{config.log_level}")
+    print("Configuration Loaded Successfully.")
