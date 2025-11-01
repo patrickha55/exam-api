b5fb9752c1f6ee5284fca49c5f0a9b5af106f1c4
patrickha55 <pattrickha@gmail.com>
2025-11-01 06:45:22 +0000

    Enhance error handling in question creation process

--- app/services/exam_service.py
+++ app/services/exam_service.py
@@ -1,7 +1,30 @@
+import logging
+from fastapi import HTTPException
+from app.data_access.exam import get_exam
+from app.data_access.question import create_question
+from app.dto.question_dto import QuestionDto
 
 
-# def create_exam(question: Question) -> None:
-#     try:
-#         exam = get
-#     except:
-#         print('An exception occurred')
+logger = logging.getLogger(__name__)
+
+
+def handle_question_creation(questionDTO: QuestionDto) -> None:
+    try:
+        if not questionDTO:
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid payload."
+            )
+
+        exam = get_exam(questionDTO.exam_id)
+
+        if exam is None:
+            raise HTTPException(
+                status_code=404,
+                detail="No exam found."
+            )
+
+        create_question(questionDTO)
+    except Exception:
+        logger.error("Error handling question creation", exc_info=True)
+        raise
