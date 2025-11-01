2b8170d70f8ae0cbc9860ef223e19040394f8408
patrickha55 <pattrickha@gmail.com>
2025-11-01 06:45:14 +0000

    Refactor exam and question data access methods to improve error handling and data retrieval

--- app/data_access/exam.py
+++ app/data_access/exam.py
@@ -1,20 +1,38 @@
+import datetime
+from datetime import datetime
+import logging
 from typing import Any
 from app.config.main import get_settings
 import psycopg
 
+from app.data_access.question import get_questions_by_exam_id
 from app.models.exam import Exam
 
 SETTINGS = get_settings()
+logger = logging.getLogger(__name__)
 
 
 def get_exams() -> Any:
-    to_return = []
-    print(SETTINGS)
+    try:
+        to_return: list[Exam] = []
+        query: str = "SELECT * FROM exam.exams"
 
-    with psycopg.connect(SETTINGS.postgres_url) as conn:
-        to_return = conn.execute("SELECT * FROM exam.exams").fetchall()
+        with psycopg.connect(SETTINGS.postgres_url) as conn:
+            rows = conn.execute(query).fetchall()
 
-    return to_return
+            for row in rows:
+                created: datetime = row[2]
+
+                updated: datetime | None = row[3] if row[3] else None
+
+                exam = Exam(id=row[0], name=row[1], created_at=created,
+                            updated_at=updated, questions=[])
+
+                to_return.append(exam)
+
+        return to_return
+    except Exception:
+        logger.error("Error fetching exams", exc_info=True)
 
 
 def get_exam(id: int) -> Exam | None:
@@ -26,30 +44,15 @@ def get_exam(id: int) -> Exam | None:
             if row is None:
                 return None
 
-            return Exam(id=row[0], name=row[1], created_at=row[2], updated_at=row[3], questions=[])
+            created: datetime = row[2]
+            updated: datetime | None = row[3] if row[3] else None
 
-    except Exception as ex:
-        print(ex)
+            return Exam(id=row[0], name=row[1], created_at=created,
+                        updated_at=updated, questions=get_questions_by_exam_id(row[0]))
+
+    except Exception:
+        logger.error("Error fetching exam with id %s", id, exc_info=True)
         raise
-
-# def get_exam(id: int) -> Exam | None:
-#     query = "SELECT * FROM exam.exams WHERE id = %s"
-#     try:
-#         with psycopg.connect(SETTINGS.postgres_url) as conn:
-#             row = conn.execute(query, [id]).fetchone()
-#             if row is None:
-#                 return None
-
-#             # Assuming Exam model has id and name fields
-#             return Exam(
-#                 id=row[0],
-#                 name=row[1],
-#                 created_at=row[2], updated_at=row[3], questions=[]
-#             )
-
-#     except Exception as ex:
-#         print(ex)
-#         raise
 
 
 def create_exam(name: str) -> bool:
@@ -62,8 +65,8 @@ def create_exam(name: str) -> bool:
         with psycopg.connect(SETTINGS.postgres_url) as conn:
             conn.execute(query, (name,))
             conn.commit()
-    except Exception as ex:
-        print(ex)
+    except Exception:
+        logger.error("Error creating exam with name %s", name, exc_info=True)
         raise
 
     return True
--- app/data_access/question.py
+++ app/data_access/question.py
@@ -1,13 +1,48 @@
+import logging
 import psycopg
 from app.config.main import get_settings
+from app.dto.question_dto import QuestionDto
 from app.models.question import Question
 
 SETTINGS = get_settings()
 
+logger = logging.getLogger(__name__)
 
-def create_questions(question: Question) -> None:
-    query = """INSERT INTO question.questions 
-                (exam_id, name, answer_one, answer_two, answer_three, answer_three, answer_four, correct_answer, is_multi_choices)
+
+def get_questions_by_exam_id(exam_id: int) -> list[Question]:
+    query = "SELECT * FROM exam.questions WHERE exam_id = %s"
+    to_return: list[Question] = []
+    try:
+        with psycopg.connect(SETTINGS.postgres_url) as conn:
+            rows = conn.execute(query, [exam_id]).fetchall()
+
+            for row in rows:
+                q = Question(
+                    id=row[0],
+                    exam_id=row[1],
+                    name=row[2],
+                    answer_one=row[3],
+                    answer_two=row[4],
+                    answer_three=row[5],
+                    answer_four=row[6],
+                    correct_answer=row[7],
+                    is_multi_choices=row[8],
+                    created_at=row[9],
+                    updated_at=row[10] if row[10] else None
+                )
+
+                to_return.append(q)
+
+            return to_return
+    except Exception:
+        logger.error("Error fetching questions for exam_id %s",
+                     exam_id, exc_info=True)
+        raise
+
+
+def create_question(question: QuestionDto) -> None:
+    query = """INSERT INTO exam.questions 
+                (exam_id, name, answer_one, answer_two, answer_three, answer_four, correct_answer, is_multi_choices)
                 values (%s,%s,%s,%s,%s,%s,%s,%s)"""
 
     try:
@@ -23,6 +58,5 @@ def create_questions(question: Question) -> None:
                 question.is_multi_choices
             ))
             conn.commit()
-    except Exception as ex:
-        print('An exception occurred')
-        print(ex)
+    except Exception:
+        logger.error("Error creating question", exc_info=True)
